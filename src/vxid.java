2c2b2a091bb4968ade4befb07c8de8979b8784f4
alexosips <alexanderlive15092003@gmail.com>
2023-03-04 15:44:36 +0200

    Налаштована логіка входу в додаток 2.0

--- src/vxid.java
+++ src/vxid.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class vxid extends JFrame{
     private JPanel panelvx;
@@ -8,16 +9,32 @@ public class vxid extends JFrame{
     private JPasswordField PasswordField;
     private JButton Button;
     private JButton Button2;
-
+    private ArrayList<String> myarraylist = new ArrayList<String>(); //Тимчасовий список
+    private ArrayList<String> password = new ArrayList<String>();
     public vxid(){
         this.getContentPane().add(panelvx);
         this.Button2.addActionListener(new MyButtn());
+        this.Button.addActionListener(new MyButtn2());
+        myarraylist.add("alex");
+        password.add("root");
     }
     private class MyButtn implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
             System.exit(0);
+        }
+    }
+    private class MyButtn2 implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String f = new String(PasswordField.getPassword());
+            if ( myarraylist.contains(TextField.getText()) &&password.contains(f)){
+                System.out.print("ok");
+            } else {
+                JOptionPane.showMessageDialog(panelvx,"Не вірний логін або пароль!");
+            }
         }
     }
 }
