2bfd7f3c7e11403d24a5cc4365c746ab0aed5564
alexosips <alexanderlive15092003@gmail.com>
2023-03-04 15:23:34 +0200

    Налаштована логіка входу в додаток

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //vxid vxid = new vxid();
-        //vxid.pack();
-        //vxid.setSize(300,200);
-        //vxid.setVisible(true);
-        //vxid.setDefaultCloseOperation(vxid.EXIT_ON_CLOSE);
-        //vxid.setResizable(false);
-        //vxid.setLocationRelativeTo(null);
+        vxid vxid = new vxid();
+        vxid.pack();
+        vxid.setSize(300,200);
+        vxid.setVisible(true);
+        vxid.setDefaultCloseOperation(vxid.EXIT_ON_CLOSE);
+        vxid.setResizable(false);
+        vxid.setLocationRelativeTo(null);
         String userName = "root";
         String password = "20032021";
         String coonectionUrl = "jdbc:mysql://localhost:3306/zaprav";
